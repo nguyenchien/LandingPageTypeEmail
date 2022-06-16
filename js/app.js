2db212da3d543e1bb2b1967bb06af5e68329686b
nguyenchien <chiennguyen1702@gmail.com>
2022-06-16 08:50:56 +0700

    commit code

--- js/app.js
+++ js/app.js
@@ -19,18 +19,32 @@ var cdata = getCookie("username");
 console.log(cdata);
 
 $(document).ready(function () {
-  $(document).ready(function () {
-    $("#owl-demo").owlCarousel({
-      navigation: true,
-      pagination: false,
-      navigationText: false,
-      slideSpeed: 300,
-      paginationSpeed: 400,
-      items: 1,
-      itemsDesktop: false,
-      itemsDesktopSmall: false,
-      itemsTablet: false,
-      itemsMobile: false,
-    });
+  // Slider
+  $("#owl-demo").owlCarousel({
+    navigation: true,
+    pagination: false,
+    navigationText: false,
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    items: 1,
+    // itemsDesktop: false,
+    // itemsDesktopSmall: false,
+    // itemsTablet: false,
+    // itemsMobile: false,
+  });
+
+  //Tab
+  $(".content-list:not(.active)").hide();
+  $(".tab-item a").on("click", function (e) {
+    e.preventDefault();
+    let targetList = $(this).attr("href");
+    $(this).parent(".tab-item").find("a").addClass("active");
+    $(this)
+      .parent(".tab-item")
+      .siblings(".tab-item")
+      .find("a")
+      .removeClass("active");
+    $(targetList).addClass("active").siblings().removeClass("active");
+    $(targetList).show().siblings().hide();
   });
 });
