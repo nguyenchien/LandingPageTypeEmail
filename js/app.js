4c70cc9f3dd3ce73f536896e0ac646aee8fb0a78
nguyenchien <chiennguyen1702@gmail.com>
2022-06-16 15:01:37 +0700

    commit code

--- js/app.js
+++ js/app.js
@@ -27,9 +27,25 @@ $(document).ready(function () {
     slideSpeed: 300,
     paginationSpeed: 400,
     items: 1,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 5,
+        nav: true,
+        loop: false,
+      },
+    },
     // itemsDesktop: false,
     // itemsDesktopSmall: false,
-    // itemsTablet: false,
+    // itemsTablet: true,
     // itemsMobile: false,
   });
 
@@ -46,5 +62,21 @@ $(document).ready(function () {
       .removeClass("active");
     $(targetList).addClass("active").siblings().removeClass("active");
     $(targetList).show().siblings().hide();
+  });
+  // Menu SP
+  $(".header__icon-menu").on("click", function (e) {
+    $(this).siblings(".header__wrap-menu").addClass("active");
+    $("body").addClass("overlay");
+  });
+  $(".icon-close").on("click", function () {
+    $(this).parent(".header__wrap-menu").removeClass("active");
+    $("body").removeClass("overlay");
+  });
+  $(document).mouseup(function (e) {
+    var targetClass = $(e.target)[0].className;
+    if (targetClass == "overlay") {
+      $("body").removeClass("overlay");
+      $(".header__wrap-menu").removeClass("active");
+    }
   });
 });
